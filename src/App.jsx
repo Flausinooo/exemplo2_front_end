11b0705aaaa6ed85eab8284fc5c6880c688e8e5b
Gabriel Flausino <gabriel.ferreira.flausino@gmail.com>
2026-08-20 09:46:15 -0300

    FEAT:ajeitando o App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 
+import Aluno from'./components/Aluno'
+import Dados from'./components/Dados'
+import Contador from'./components/Contador'
+
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    //fragments
+    <>
+      <Aluno id=" RM572486" nome=" Gabriel Flausino " email=" gabriel.ferreira.flausino " telefone=" 11 94715-9307"/>
+      <Dados nome=" Gabriel Ferreira" descricao=" Futuro Engenheiro de Software" data="20/08/2026"/>
+      <Contador/>
+  </>
   )
 }
 
