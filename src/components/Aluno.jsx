53517ae942cfd150c748c1aba7fff7a0ee5380a1
Gabriel Flausino <gabriel.ferreira.flausino@gmail.com>
2026-08-20 08:52:59 -0300

    feat:Componente utilizando props

---
+++ src/components/Aluno.jsx
@@ -0,0 +1,11 @@
+
+const Aluno = (props) => {
+  return (
+    <>
+      <p>O Nome do Aluno é:{props.nome} </p>
+      <p>A Idade do Aluno é:{props.idade}</p>
+    </>
+  )
+}
+
+export default Aluno
